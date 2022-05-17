2f5b07a3074fabc0a1cf7683be8abbf4d9a47f49
Santo Blasius <santoblasius@atech.software>
2022-05-17 12:22:32 +0700

    Update openapi anotation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,11 @@ use App\Http\Requests\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Tag(name="auth")
+ */
 class AuthController extends Controller
 {
     public function __construct()
@@ -15,6 +19,51 @@ class AuthController extends Controller
         $this->middleware('auth:sanctum')->only(['authUser', 'logout']);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/auth/login",
+     *     tags={"auth"},
+     *     @OA\Parameter(
+     *          name="body",
+     *          in="path",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="email", type="string", format="email", example="john.snow@stark.com"),
+     *              @OA\Property(property="password", type="string", example="secret"),
+     *              @OA\Property(property="device_name", type="string", example="web")
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=200,
+     *          description="Login success",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(property="token", type="string"),
+     *                  @OA\Property(property="user", type="object", ref="#/components/schemas/User")
+     *              ),
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=400,
+     *          description="Login failed",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="error",
+     *                  type="object",
+     *                  @OA\Property(property="code", type="number", example="401"),
+     *                  @OA\Property(property="message", type="string", example="The provided credentials are incorrect.")
+     *              )
+     *          )
+     *     )
+     * )
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(LoginRequest $request)
     {
         $user = User::where('email', $request->email)->first();
@@ -22,10 +71,10 @@ class AuthController extends Controller
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json([
                 'error' => [
-                    'code' => '',
+                    'code' => 401,
                     'message' => 'The provided credentials are incorrect.',
                 ]
-            ]);
+            ], 400);
         }
 
         $token = $user->createToken($request->device_name)->plainTextToken;
@@ -38,6 +87,19 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     tags={"auth"},
+     *     @OA\Response(
+     *          response=204,
+     *          description="Logout success"
+     *     )
+     * )
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function logout(Request $request)
     {
         $request->user()->tokens()->delete();
@@ -45,6 +107,35 @@ class AuthController extends Controller
         return response(null, 204);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/auth/user",
+     *     tags={"auth"},
+     *     @OA\Response(
+     *          response=200,
+     *          description="Authenticated user",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(
+     *                  property="data",
+     *                  type="object",
+     *                  @OA\Property(property="user", type="object", ref="#/components/schemas/User")
+     *              ),
+     *          )
+     *     ),
+     *     @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *          @OA\JsonContent(
+     *              type="object",
+     *              @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *          )
+     *     )
+     * )
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function authUser(Request $request)
     {
         return response()->json([
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,11 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations\OpenApi as OA;
 
+/**
+ * @OA\Info(version="1.0.0", title="CityU Homage")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
